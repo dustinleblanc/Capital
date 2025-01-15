65b11008f55aec7da71d29c3fe2f4fd764fed0fb
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2025-01-15 17:53:57 -0500

    PSR12 formatting

--- src/Entity/Commodity.php
+++ src/Entity/Commodity.php
@@ -6,46 +6,53 @@ use App\Repository\CommodityRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CommodityRepository::class)]
-class Commodity implements CommodityInterface {
-  #[ORM\Id]
-  #[ORM\GeneratedValue]
-  #[ORM\Column]
-  private ?int $id = null;
+class Commodity implements CommodityInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
 
-  #[ORM\Column(nullable: true)]
-  private ?float $labor = null;
+    #[ORM\Column(nullable: true)]
+    private ?float $labor = null;
 
-  #[ORM\Column(length: 255, nullable: true)]
-  private ?string $useValue = null;
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $useValue = null;
 
-  public function getId(): ?int {
-    return $this->id;
-  }
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
-  public function getLabor(): ?float {
-    return $this->labor;
-  }
+    public function getLabor(): ?float
+    {
+        return $this->labor;
+    }
 
-  public function setLabor(?float $labor): static {
-    $this->labor = $labor;
+    public function setLabor(?float $labor): static
+    {
+        $this->labor = $labor;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  public function getUseValue(): ?string {
-    return $this->useValue;
-  }
+    public function getUseValue(): ?string
+    {
+        return $this->useValue;
+    }
 
-  public function setUseValue(?string $useValue): static {
-    $this->useValue = $useValue;
+    public function setUseValue(?string $useValue): static
+    {
+        $this->useValue = $useValue;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  /**
-   * {@inheritDoc}
-   */
-  public function getRelativeValue(CommodityInterface $commodity, int $quantity): float {
-    return $commodity->getLabor() / ($this->getLabor() * $quantity);
-  }
+    /**
+     * {@inheritDoc}
+     */
+    public function getRelativeValue(CommodityInterface $commodity, int $quantity): float
+    {
+        return $commodity->getLabor() / ($this->getLabor() * $quantity);
+    }
 }
